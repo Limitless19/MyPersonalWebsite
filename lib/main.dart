d3bf704a8c86bcf14b7ed7fea96c510ba182e416
Tolulope Oke <33998304+Limitless19@users.noreply.github.com>
2020-01-29 14:38:56 +0100

    Web build and configurations

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -22,12 +22,18 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          style: TextStyle(
-            color: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('My Personal Website'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
         ),
       ),
